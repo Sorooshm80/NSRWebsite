d4eb7cd905e6799989d53618dd23a275822f80cc
Sorooshm80 <sorooshm80@gmail.com>
2025-01-29 18:54:24 +0330

    Added global , index and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './components/global/index/index.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    IndexComponent,
+    MatGridListModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
